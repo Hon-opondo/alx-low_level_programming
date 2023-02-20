297b7fd08458929bb6d1a7197002a1fe45dc8cd5
Hon-opondo <johnhongo61@gmail.com>
2023-02-20 07:06:28 -0800

    never thought that alphabet soup would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ * except for letters e and q
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a = 97;
+
+	while (a <= 122)
+	{
+		if (a == 101 || a == 113)
+		{
+			a++;
+			continue;
+		}
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
