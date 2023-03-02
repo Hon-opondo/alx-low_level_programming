5c040195e06fdff338d30aa221477783be8536f3
Hon-opondo <johnhongo61@gmail.com>
2023-03-02 02:05:00 -0800

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * string_toupper - converts all lowercasae to uppercase
+ * @b: string
+ * Return: uppercase string
+ */
+char *string_toupper(char *b)
+{
+	int a = 0;
+
+	while (b[a])
+	{
+		b[a] = toupper(b[a]);
+		a++;
+	}
+	return (b);
+}
